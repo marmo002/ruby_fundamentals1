5c3738c33d78524fabffc329e1ff9d850c9dd874
marmo002 <marmo_02@hotmail.com>
2017-09-18 16:04:38 -0400

    ask user for edge, convert string to integer, and subtract 2017 minus their edge to give year they were born

--- exercise3.rb
+++ exercise3.rb
@@ -1,3 +1,7 @@
 puts "What is your name"
-user_name = gets
+  user_name = gets
 puts "Hello, #{user_name}"
+puts "How old are you?"
+  user_age = gets
+  user_age = user_age.to_i
+puts "So you were born on #{2017 - user_age}"
