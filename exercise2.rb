32f2af76e2f5dda8b27d7871c884b4361ea8ce29
marmo002 <marmo_02@hotmail.com>
2017-09-18 15:07:25 -0400

    do exercise 2 questions 1 to 4

---
+++ exercise2.rb
@@ -0,0 +1,20 @@
+#QUESTION 1
+price = 98
+tip = price * 0.1
+
+puts "For my $#{price}.00 dinner, I will give a tip of $#{tip}"
+
+#QUESTION 2
+string_number = "2"
+times_number = 2
+addition = string_number.to_i + times_number
+puts "add string \"#{string_number}\" + integer #{times_number} = #{addition}"
+
+#QUESTION 3
+puts "the result of 45628 times 7839 is equal to #{45628 * 7839}"
+
+#QUESTION 4
+
+#anwser is true, I think ...
+
+puts "#{(10 < 20 && 30 < 20) || !(10 == 11)}"
