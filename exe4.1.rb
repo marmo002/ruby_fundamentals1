a01a01ce4de69e5555ba3515d92282ea1f7f085d
marmo002 <marmo_02@hotmail.com>
2017-09-18 16:29:40 -0400

    do exercise4 question1

---
+++ exe4.1.rb
@@ -0,0 +1,8 @@
+puts "Please give me a number"
+number_1 = gets.to_i
+
+if number_1 >= 100
+  puts "that's a big number"
+else
+  puts "Why not dream a litle bigger"
+end
--- exercise3.rb
+++ exercise3.rb
@@ -2,6 +2,9 @@ puts "What is your name"
   user_name = gets
 puts "Hello, #{user_name}"
 puts "How old are you?"
-  user_age = gets
-  user_age = user_age.to_i
+  # user_age = gets
+  # user_age = user_age.to_i
+
+  user_age = gets.to_i
+  
 puts "So you were born on #{2017 - user_age}"
